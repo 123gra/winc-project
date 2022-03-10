00f4e1e054c9d17652361ff599620374d10ac423
K <priyankakk@deloitte.com>
2022-03-10 20:35:39 +0530

    base screens

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserLoginComponent } from './login/user-login/user-login/user-login.component';
+import { UserRegistrationComponent } from './login/user-registration/user-registration/user-registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserLoginComponent,
+    UserRegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
