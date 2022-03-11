d05258a671af70ec6577aa036d7c7f2ceabdd378
firdose12 <firdusmiles@gmail.com>
2022-03-11 20:10:53 +0530

    login/registratio/events Screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {APP_BASE_HREF} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './login/user-login/user-login/user-login.component';
 import { UserRegistrationComponent } from './login/user-registration/user-registration/user-registration.component';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLoginComponent,
-    UserRegistrationComponent
+    UserRegistrationComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: ''}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
